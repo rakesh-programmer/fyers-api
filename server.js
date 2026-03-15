1475a7ab5d0e24c550a0b3336583b73183b599fe
rakesh-programmer <rakesh.programmer@aol.com>
2026-03-16 00:47:24 +0530

    testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Starting automated trading engine...');
+  console.log('A temporary login page has been created for testing purposes. Please log in to access the trading dashboard.');
   console.log(`Automated trading engine running on port ${PORT}`);
   console.log(`Log in here: http://static_ip:${PORT}/login`);
 });
