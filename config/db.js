e6649f57c23bf182f40fb9de52d3bc05f6a93217
rakesh-programmer <rakesh.programmer@aol.com>
2026-03-16 20:42:47 +0530

    db integrated

---
+++ config/db.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+const DEFAULT_MONGODB_URI_TEMPLATE =
+  'mongodb+srv://<db_username>:<db_password>@rakesh.f82ecsa.mongodb.net/?retryWrites=true&w=majority&appName=Rakesh';
+
+const buildMongoUri = () => {
+  const username = process.env.DB_USER;
+  const password = process.env.DB_PASS;
+  const template = process.env.MONGODB_URI_TEMPLATE || DEFAULT_MONGODB_URI_TEMPLATE;
+
+  if (!username) {
+    throw new Error('DB_USER is not set in the environment.');
+  }
+
+  if (!password) {
+    throw new Error('DB_PASS is not set in the environment.');
+  }
+
+  let mongoUri = template;
+
+  if (mongoUri.includes('<db_username>')) {
+    mongoUri = mongoUri.replace('<db_username>', encodeURIComponent(username));
+  }
+
+  if (mongoUri.includes('<db_password>')) {
+    mongoUri = mongoUri.replace('<db_password>', encodeURIComponent(password));
+  }
+
+  return mongoUri;
+};
+
+const connectToDatabase = async () => {
+  const mongoUri = buildMongoUri();
+  console.log('Connecting to MongoDB with URI:', mongoUri);
+
+ try {
+   await mongoose.connect(mongoUri, {
+    dbName: process.env.MONGODB_DB_NAME || 'fyers-api'
+  });
+
+  console.log('Connected to MongoDB');
+ } catch (error) {
+  console.error('Error connecting to MongoDB:', error.message);
+ }
+};
+
+module.exports = {
+  connectToDatabase
+};
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
 
 const app = require('./app');
+const { connectToDatabase } = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('A temporary login page has been created 123');
-  console.log(`Automated trading engine running on port ${PORT}`);
-  console.log(`Log in here: http://static_ip:${PORT}/login`);
-});
+const startServer = async () => {
+  try {
+    await connectToDatabase();
+
+    app.listen(PORT, () => {
+      console.log('A temporary login page has been created 123');
+      console.log(`Automated trading engine running on port ${PORT}`);
+      console.log(`Log in here: http://static_ip:${PORT}/login`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
